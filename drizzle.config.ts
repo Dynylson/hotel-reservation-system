a1eae310f5862e2df83f6740eaf0e4a001ed0215
Dyn Júnior <dynylsonjr@gmail.com>
2025-03-31 22:03:22 -0300

    chore: config drizzle

---
+++ drizzle.config.ts
@@ -0,0 +1,12 @@
+import 'dotenv/config';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { defineConfig } from 'drizzle-kit';
+
+export default defineConfig({
+  out: './drizzle',
+  schema: './src/db/schema.ts',
+  dialect: 'postgresql',
+  dbCredentials: {
+    url: process.env.DATABASE_URL || '',
+  },
+});
